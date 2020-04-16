8150913fe45c96c554fc657bdef4b62ac44b6cb6
dudj <13020078873@163.com>
2020-04-16 16:32:53 +0800

    负载均衡ribbon整合

--- README.md
+++ README.md
@@ -8,3 +8,25 @@ discovery-eureka 与 discovery-eureka-ha 两者之间通过域名做了互相注
 discovery-eureka-authenticating进行了security的验证 使用了config配置
 
 comsumer-movie和provider-user 两者通过用户名和密码向discovery-eureka-authenticating进行注册
+
+
+----------------------------
+整合ribbon的方法
+引入ribbon依赖
+<dependency>    
+    <groupId>org.springframework.cloud</groupId>    
+    <artifactId>spring-cloud-starter-ribbon</artifactId>
+</dependency>
+如果依赖中添加了spring-cloud-starter-eureka，就无须再次引入，里面包含
+
+为RestTemplate添加@LoadBalanced
+
+我已经封装过restTemplate 直接添加即可
+@Bean
+    @LoadBalanced
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
+    
+    
+    -----------------------------
--- microservice-consumer-movie/src/main/java/com/ld/microserviceconsumermovie/controller/MovieController.java
+++ microservice-consumer-movie/src/main/java/com/ld/microserviceconsumermovie/controller/MovieController.java
@@ -1,17 +1,34 @@
 package com.ld.microserviceconsumermovie.controller;
 
 import com.ld.microserviceconsumermovie.vo.UserVo;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/movie")
+@Slf4j
 public class MovieController {
     @Autowired
     private RestTemplate restTemplate;
+    @Autowired
+    private LoadBalancerClient loadBalancerClient;
     @GetMapping("/index/{id}")
     public UserVo index(@PathVariable Integer id){
         return this.restTemplate.getForObject("http://microservice-provider-user/user/index/" + id, UserVo.class);
+    }
+
+    /**
+     * 通过自带ribbon进行负载均衡调用 我通过在idea中配置-Dserver-port=端口号启动了多个用户服务
+     * 可以看到每次请求会请求不同的服务，通过log或者此方法都可以看到
+     */
+    @GetMapping("log-instance")
+    public void logUserInstance(){
+        ServiceInstance serviceInstance = this.loadBalancerClient.choose("microservice-provider-user");
+        serviceInstance.getServiceId();
+        log.info("serviceId:{},host:{},port:{}",serviceInstance.getServiceId(), serviceInstance.getHost(), serviceInstance.getPort());
     }
 }
