1f0ad0d106114de2613c3baee9c6843d2cfdadac
dudj <13020078873@163.com>
2020-04-22 16:52:38 +0800

    使用hystrix dashboard 进行监控

--- microservice-consumer-movie/src/main/java/com/ld/microserviceconsumermovie/ConsumerMovieApplication.java
+++ microservice-consumer-movie/src/main/java/com/ld/microserviceconsumermovie/ConsumerMovieApplication.java
@@ -1,15 +1,18 @@
 package com.ld.microserviceconsumermovie;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 //加入openfeign的注解
-//@EnableFeignClients
+@EnableFeignClients
 @EnableHystrix
 public class ConsumerMovieApplication {
 
@@ -17,4 +20,19 @@ public class ConsumerMovieApplication {
         SpringApplication.run(ConsumerMovieApplication.class, args);
     }
 
+    /**
+     * 低版本直接启动即可使用 http://ip:port/hystrix.stream 查看监控信息
+     * 高版本需要添加本方法方可使用 http://ip:port/hystix.stream 查看监控信息
+     *
+     * @return
+     */
+    @Bean
+    public ServletRegistrationBean getServlet(){
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
+    }
 }
--- microservice-consumer-movie/src/main/java/com/ld/microserviceconsumermovie/openfeign/UserOpenFeignClient.java
+++ microservice-consumer-movie/src/main/java/com/ld/microserviceconsumermovie/openfeign/UserOpenFeignClient.java
@@ -1,6 +1,6 @@
 package com.ld.microserviceconsumermovie.openfeign;
 
-import com.ld.microserviceconsumermovie.fallback.FeignClientFallback;
+import com.ld.microserviceconsumermovie.fallback.FeignClientFallbackFactory;
 import com.ld.microserviceconsumermovie.vo.UserVo;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,8 +11,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
  * name 可以是任意的客户端名称
  * 使用@FeignClient的fallback属性指定回退类
  */
-@FeignClient(name = "microservice-provider-user", fallback = FeignClientFallback.class)
+@FeignClient(name = "microservice-provider-user-nosecurity", fallbackFactory = FeignClientFallbackFactory.class)
 public interface UserOpenFeignClient {
-    @RequestMapping(value = "/user/index/{id}", method = RequestMethod.GET)
+    @RequestMapping(value = "/user-nosecurity/index/{id}", method = RequestMethod.GET)
     public UserVo findById(@PathVariable("id") Integer id);
 }
