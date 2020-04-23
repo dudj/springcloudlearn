5533460c65d38ee9bade63727050bebdcc0245cc
dudj <13020078873@163.com>
2020-04-23 14:06:50 +0800

    zuul 网关

---
+++ microservice-gateway-zuul/src/test/java/com/ld/microservicegatewayzuul/MicroserviceGatewayZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ld.microservicegatewayzuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceGatewayZuulApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
